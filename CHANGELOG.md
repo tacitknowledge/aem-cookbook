68f10d8539f24564574b1a13bdce3661dcf5e034
Paul Dunnavant <pdunnavant@tacitknowledge.com>
2014-06-02 10:57:44 -0700

    Bump version to 1.0.1; add changelog.

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# 1.0.1
+
+* Enhancements
+  * Added support for AEM 6.0.0.
+
+# 1.0.0
+
+* Initial release to Github.
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ license          'Apache 2.0'
 name             'aem'
 description      'Installs/Configures Adobe AEM'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.0.1'
 
 recipe 'aem::author', 'Installs AEM Author instance.'
 recipe 'aem::publish', 'Installs AEM Publish instance.'
